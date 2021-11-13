15ba736e0bf898febe1749539d654b96bcdedc41
Melodi <62381002+Melodi17@users.noreply.github.com>
2021-11-13 15:00:34 +1000

    Update Program.cs

--- FileTransfer/Program.cs
+++ FileTransfer/Program.cs
@@ -7,6 +7,7 @@ namespace FileTransfer
         static void Main(string[] args)
         {
             Console.WriteLine("I changed it twice!");
+            // This code is a bit sus...
         }
     }
 }
