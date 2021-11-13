16d2ee468b7e0824cc4d4c43d584f4a8364b240e
Melodi <62381002+Melodi17@users.noreply.github.com>
2021-11-13 17:50:10 +1000

    Update FileTransferService.cs

--- File Transfer 2/FileTransferService.cs
+++ File Transfer 2/FileTransferService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,8 @@ namespace File_Transfer_2
 {
     public static class FileTransferService
     {
-        public static int Port;
+        public static int Port = 5001;
+        public static string DownloadPath;
         public static TCPConnectionServer Server;
         /* Only 'local' variables start with a lower-case letter, these are global because
          * they can be accessed anywhere in the code. Local variables are like the stuff
@@ -19,8 +21,8 @@ namespace File_Transfer_2
         /* A dictionary is like a list but instead of an index like this mylist[0] we do this
          * mydictionary["key"] so the 'index' is like the key to get the value
          * the long is basically just a number that can have a really high value
-         * higher than an int, but it can only have positive numbers and 0. We probably won't
-         * end up using this Dictionary but it is good practice so you understand. */
+         * higher than an int, but it can only have positive numbers and 0. */
+        public static Dictionary<long, string> ConnectionFileName;
         public static byte[][] SplitForSending(byte[] buffer, int blockSize)
         {
             byte[][] blocks = new byte[(buffer.Length + blockSize - 1) / blockSize][];
@@ -43,6 +45,7 @@ namespace File_Transfer_2
         }
         public static void Init()
         {
+            DownloadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
             Server = new TCPConnectionServer(Port);
 
             Server.onConnect = client =>
@@ -52,7 +55,7 @@ namespace File_Transfer_2
              * part is what we choose to name the argument passed to us. */
             {
                 DialogResult result = MessageBox.Show($"{client.Client.RemoteEndPoint} would like to send you a file", "File sharing request", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-                if (result == DialogResult.OK)
+                if (result == DialogResult.Yes)
                 {
                     Server.Send(client, "Accepted");
                     ConnectionRequestAccept[client.SocketId()] = true;
@@ -66,16 +69,28 @@ namespace File_Transfer_2
             };
             Server.onDisconnect = client =>
             {
-                /* Don't Care (For now) */
+                /* Delete connection record to free up space */
             };
             Server.onMessage = (client, message) =>
             {
-                if (ConnectionRequestAccept.ContainsKey(client.SocketId()))
+                long clientID = client.SocketId();
+                if (ConnectionRequestAccept.ContainsKey(clientID))
                 {
-                    if (ConnectionRequestAccept[client.SocketId()])
+                    if (ConnectionRequestAccept[clientID])
                     /* Makes sure we accepted the connection before allowing download */
                     {
-
+                        if (ConnectionFileName.ContainsKey(clientID))
+                        /* Work out if this is the first message, and if so, it will
+                         * Set the filename to this message. If not, it will*/
+                        {
+                            File.AppendAllText(Path.Combine(DownloadPath, ConnectionFileName[clientID]), message);
+                        }
+                        else
+                        {
+                            ConnectionFileName[clientID] = message;
+                            File.WriteAllText(Path.Combine(DownloadPath, ConnectionFileName[clientID]), "");
+                            /* Generate empty file */
+                        }
                     }
                 }
             };
